dd18e083522d2e8a0aa555f9e6ddfc9ef10f9a7b
Margret Riegert <margret@eowyn.net>
2025-01-09 16:53:32 -0500

    Remove warning using regex

--- bot.rb
+++ bot.rb
@@ -106,15 +106,8 @@ def parse_code_tree_sitter(code)
       stdout = io.read
     end
 
-    warning = <<-WARNING
-    Warning: You have not configured any parser directories!
-    Please run `tree-sitter init-config` and edit the resulting
-    configuration file to indicate where we should look for
-    language grammars.
-    WARNING
-
     if $?.success?
-      [commit_hash, stdout.gsub(warning, "")]
+      [commit_hash, stdout.gsub(/Warning:(.|\n)*\n\n/m, "")]
     else
       stderr = stdout
       [commit_hash, stderr]
